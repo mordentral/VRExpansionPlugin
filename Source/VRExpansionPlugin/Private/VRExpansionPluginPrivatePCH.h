13c3648aba884adbb6d8537f1c1364e8a46f6390
morden <morden@noxhub.net>
2016-07-28 20:35:29 -0400

    rePrep

--- Source/VRExpansionPlugin/Private/VRExpansionPluginPrivatePCH.h
+++ Source/VRExpansionPlugin/Private/VRExpansionPluginPrivatePCH.h
@@ -7,7 +7,7 @@
 #include "ReplicatedVRCameraComponent.h"
 #include "ParentRelativeAttachmentComponent.h"
 #include "VRRootComponent.h"
-#include "VRMovementComponent.h"
+//#include "VRMovementComponent.h"
 
 // You should place include statements to your module's private header files here.  You only need to
 // add includes for headers that are used in most of your module's source files though.
