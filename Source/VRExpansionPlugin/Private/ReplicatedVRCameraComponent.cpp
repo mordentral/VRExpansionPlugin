545cb80f71eb1735c588c17341f3da89f31ed366
morden <morden@noxhub.net>
2016-08-27 20:24:49 -0400

    Fixed missing header file in VRCameraComponent

--- Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
+++ Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #include "VRExpansionPluginPrivatePCH.h"
+#include "Net/UnrealNetwork.h"
 #include "ReplicatedVRCameraComponent.h"
 
 
