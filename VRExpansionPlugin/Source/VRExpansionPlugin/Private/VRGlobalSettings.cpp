45222c6f630ec036c5230da09f301c244d67f23a
mordentral <morden@noxhub.net>
2018-01-30 09:42:14 -0500

    Fixed the constructor for the VRGlobalSettings
    Added some utility functions for Controller profiles (delete, add, replace, save config).
    
    Added the option to not set a profile you are loading to current (layered profiles
    like for right/left handiness layered over a default profile).

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRGlobalSettings.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRGlobalSettings.cpp
@@ -3,8 +3,11 @@
 
 UVRGlobalSettings::UVRGlobalSettings(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer),
+	CurrentControllerProfileInUse(NAME_None),
+	CurrentControllerProfileTransform(FTransform::Identity),
 	OneEuroMinCutoff(2.0f),
 	OneEuroCutoffSlope(0.007f),
 	OneEuroDeltaCutoff(1.0f)
+
 {
 }
