c0655861c77d2557a15b3baab227efd7711f0309
mordentral <morden@noxhub.net>
2018-10-04 14:16:30 -0400

    Making client side movement attachment grips attach on all

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -1918,7 +1918,7 @@ bool UGripMotionControllerComponent::NotifyGrip(FBPActorGripInformation &NewGrip
 		if (bHasMovementAuthority)
 			TeleportMoveGrip(NewGrip);
 
-		if(HasGripAuthority(NewGrip) || IsServer())
+		if(bHasMovementAuthority || IsServer())
 			root->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
 
 	}break;
