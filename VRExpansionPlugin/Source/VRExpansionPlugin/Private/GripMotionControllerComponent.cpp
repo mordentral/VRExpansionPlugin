1a6569dd9f4a6f8b201c3e791d17d7ac404988bd
mordentral <morden@noxhub.net>
2018-10-08 11:49:34 -0400

    turning off physics sim locally as well on socketing operations
    appears that Epics sim can fail to rep

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -1662,6 +1662,9 @@ void UGripMotionControllerComponent::DropAndSocket_Implementation(const FBPActor
 				if ((NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings && NewDrop.AdvancedGripSettings.PhysicsSettings.bTurnOffGravityDuringGrip) ||
 					(NewDrop.GripMovementReplicationSetting == EGripMovementReplicationSettings::ForceServerSideMovement && !IsServer()))
 					root->SetEnableGravity(NewDrop.bOriginalGravity);
+				
+				// Stop Physics sim for socketing
+				root->SetSimulatePhysics(false);
 			}
 
 			if (IsServer()) //&& !bSkipFullDrop)
@@ -1699,6 +1702,9 @@ void UGripMotionControllerComponent::DropAndSocket_Implementation(const FBPActor
 			if ((NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings && NewDrop.AdvancedGripSettings.PhysicsSettings.bTurnOffGravityDuringGrip) ||
 				(NewDrop.GripMovementReplicationSetting == EGripMovementReplicationSettings::ForceServerSideMovement && !IsServer()))
 				root->SetEnableGravity(NewDrop.bOriginalGravity);
+
+			// Stop Physics sim for socketing
+			root->SetSimulatePhysics(false);
 
 			if (pActor)
 			{
