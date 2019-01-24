1271229bb537add3677a842b2544b1735b498632
mordentral <morden@noxhub.net>
2019-01-24 09:45:50 -0500

    Added a BP Accessible IsLocallyControlled to the motion controllers, it can be very useful.

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -2399,6 +2399,11 @@ void UGripMotionControllerComponent::Drop_Implementation(const FBPActorGripInfor
 	OnDroppedObject.Broadcast(DropBroadcastData);
 }
 
+bool UGripMotionControllerComponent::BP_IsLocallyControlled()
+{
+	return IsLocallyControlled();
+}
+
 bool UGripMotionControllerComponent::BP_HasGripAuthority(const FBPActorGripInformation &Grip)
 {
 	return HasGripAuthority(Grip);
