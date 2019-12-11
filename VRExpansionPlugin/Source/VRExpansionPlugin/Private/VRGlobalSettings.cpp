69b0d1a68da42693860b545a59183973d290c3e7
mordentral <morden@noxhub.net>
2019-12-11 15:46:08 -0500

    Added a CCDMaxPasses var to VRGlobalSettings

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRGlobalSettings.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRGlobalSettings.cpp
@@ -3,6 +3,7 @@
 
 UVRGlobalSettings::UVRGlobalSettings(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer),
+	MaxCCDPasses(1),
 	OneEuroMinCutoff(2.0f),
 	OneEuroCutoffSlope(0.007f),
 	OneEuroDeltaCutoff(1.0f),
