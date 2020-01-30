ff65b0921735f1b464e379be02b789647af24f5d
mordentral <morden@noxhub.net>
2020-01-30 11:30:53 -0500

    fixed mistake with my skip simulating code using the wrong comparisons
    also fixed auto drop using the old constraint frame and being wrong.

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -1990,8 +1990,7 @@ void UGripMotionControllerComponent::DropAndSocket_Implementation(const FBPActor
 					root->SetEnableGravity(NewDrop.bOriginalGravity);
 				
 				// Stop Physics sim for socketing
-				if (NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings && !NewDrop.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
-					root->SetSimulatePhysics(false);
+				root->SetSimulatePhysics(false);
 			}
 
 			if (IsServer()) //&& !bSkipFullDrop)
@@ -2497,7 +2496,7 @@ void UGripMotionControllerComponent::Drop_Implementation(const FBPActorGripInfor
 					{
 						if (IsServer() || bHadGripAuthority || !NewDrop.bOriginalReplicatesMovement || !pActor->GetIsReplicated())
 						{
-							if (NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings && !NewDrop.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
+							if (!NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings || !NewDrop.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
 							{
 								if (root->IsSimulatingPhysics() != bSimulate)
 								{
@@ -2604,7 +2603,7 @@ void UGripMotionControllerComponent::Drop_Implementation(const FBPActorGripInfor
 					// Need to set simulation in all of these cases, including if it isn't the root component (simulation isn't replicated on non roots)
 					if (IsServer() || bHadGripAuthority || !NewDrop.bOriginalReplicatesMovement || (pActor && (pActor->GetRootComponent() != root || !pActor->GetIsReplicated())))
 					{
-						if (NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings && !NewDrop.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
+						if (!NewDrop.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings || !NewDrop.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
 						{
 							if (root->IsSimulatingPhysics() != bSimulate)
 							{
@@ -4309,7 +4308,7 @@ bool UGripMotionControllerComponent::SetUpPhysicsHandle(const FBPActorGripInform
 
 
 	// Needs to be simulating in order to run physics
-	if (NewGrip.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings && !NewGrip.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
+	if (!NewGrip.AdvancedGripSettings.PhysicsSettings.bUsePhysicsSettings || !NewGrip.AdvancedGripSettings.PhysicsSettings.bSkipSettingSimulating)
 		root->SetSimulatePhysics(true);
 
 	/*if(NewGrip.GrippedBoneName == NAME_None)
@@ -4868,7 +4867,7 @@ bool UGripMotionControllerComponent::GetPhysicsJointLength(const FBPActorGripInf
 		return false;
 	// This is supposed to be the difference between the actor and the kinactor / constraint base
 	
-	FTransform tran3 = FPhysicsInterface::GetLocalPose(HandleInfo->HandleData2, EConstraintFrame::Frame1);
+	FTransform tran3 = FPhysicsInterface::GetLocalPose(HandleInfo->HandleData2, EConstraintFrame::Frame2);
 
 	FTransform rr;
 	FBodyInstance* rBodyInstance = rootComp->GetBodyInstance(GrippedActor.GrippedBoneName);
