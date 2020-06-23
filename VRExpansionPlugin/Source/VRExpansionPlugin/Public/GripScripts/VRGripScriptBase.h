7fcdd44ae92e0ad57c96befa4004b2e8d3f55f6f
Joshua <mordentral@gmail.com>
2020-06-23 12:10:32 -0400

    fix grip script vars not showing in defaults
    fix grip script vars not showing in defaults

--- VRExpansionPlugin/Source/VRExpansionPlugin/Public/GripScripts/VRGripScriptBase.h
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Public/GripScripts/VRGripScriptBase.h
@@ -52,14 +52,14 @@ public:
 	bool IsScriptActive();
 
 	// Is currently active helper variable, returned from IsScriptActive()
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "DefaultSettings")
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GSSettings")
 	bool bIsActive;
 
 	// Returns if the script is going to modify the world transform of the grip
 	EGSTransformOverrideType GetWorldTransformOverrideType();
 
 	// Whether this script overrides or modifies the world transform
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "DefaultSettings")
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GSSettings")
 	EGSTransformOverrideType WorldTransformOverrideType;
 
 	// Returns if the script wants auto drop to be ignored
@@ -69,7 +69,7 @@ public:
 	}
 
 	// Returns if we want to deny auto dropping
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "DefaultSettings")
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GSSettings")
 		bool bDenyAutoDrop;
 
 	// Returns if the script wants to force a drop
@@ -79,7 +79,7 @@ public:
 	}
 
 	// Returns if we want to force a drop
-	UPROPERTY(BlueprintReadWrite, Category = "DefaultSettings")
+	UPROPERTY(BlueprintReadWrite, Category = "GSSettings")
 		bool bForceDrop;
 
 	// Flags the grip to be dropped as soon as possible
@@ -95,8 +95,8 @@ public:
 		return bDenyLateUpdates;
 	}
 
-	// Returns if we want to deny late updates
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "DefaultSettings")
+	// Returns if we want to inject changes prior to the physics handle
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GSSettings")
 		bool bDenyLateUpdates;
 
 	// Returns if the script wants auto drop to be ignored
@@ -105,20 +105,20 @@ public:
 		return bInjectPrePhysicsHandle;
 	}
 
-	// Returns if we want to deny auto dropping
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "DefaultSettings")
+	// Returns if we want to inject changes prior to the physics handle
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GSSettings")
 		bool bInjectPrePhysicsHandle;
 
 	virtual void HandlePrePhysicsHandle(UGripMotionControllerComponent* GrippingController, const FBPActorGripInformation &GripInfo, FBPActorPhysicsHandleInformation * HandleInfo, FTransform & KinPose);
 
-	// Returns if the script wants auto drop to be ignored
+	// Returns if we want to inject changes after the physics handle
 	FORCEINLINE bool InjectPostPhysicsHandle()
 	{
 		return bInjectPostPhysicsHandle;
 	}
 
-	// Returns if we want to deny auto dropping
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "DefaultSettings")
+	// Returns if we want to inject changes after the physics handle
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GSSettings")
 		bool bInjectPostPhysicsHandle;
 
 	virtual void HandlePostPhysicsHandle(UGripMotionControllerComponent* GrippingController, FBPActorPhysicsHandleInformation * HandleInfo);
