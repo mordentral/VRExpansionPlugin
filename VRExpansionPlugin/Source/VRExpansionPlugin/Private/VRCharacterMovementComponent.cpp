08571abbfc58fec0576c2ef422bef67253dd4bbb
mordentral <morden@noxhub.net>
2020-12-09 20:27:57 -0500

    fixes snap turn rollback when using authoritative rotation
    fixes snap turn rollback when using authoritative rotation

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRCharacterMovementComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRCharacterMovementComponent.cpp
@@ -743,10 +743,10 @@ void UVRCharacterMovementComponent::ServerMoveVR_Implementation(
 	ViewRot.Yaw = FRotator::DecompressAxisFromShort(MoveReps.ClientYaw);
 	ViewRot.Roll = FRotator::DecompressAxisFromByte(MoveReps.ClientRoll);
 
-	if (PC && bUseClientControlRotation)
+/*	if (PC && bUseClientControlRotation)
 	{
 		PC->SetControlRotation(ViewRot);
-	}
+	}*/
 
 	if (!bServerReadyForClient)
 	{
@@ -756,11 +756,6 @@ void UVRCharacterMovementComponent::ServerMoveVR_Implementation(
 	// Perform actual movement
 	if ((MyWorld->GetWorldSettings()->GetPauserPlayerState() == NULL) && (DeltaTime > 0.f))
 	{
-		if (PC)
-		{
-			PC->UpdateRotation(DeltaTime);
-		}
-
 		if (!ConditionalReps.RequestedVelocity.IsZero())
 		{
 			RequestedVelocity = ConditionalReps.RequestedVelocity;
@@ -809,6 +804,23 @@ void UVRCharacterMovementComponent::ServerMoveVR_Implementation(
 
 		MoveAutonomous(TimeStamp, DeltaTime, MoveFlags, Accel);
 		bHasRequestedVelocity = false;
+
+		if (PC)
+		{
+			if (bUseClientControlRotation)
+			{
+				PC->SetControlRotation(ViewRot);
+			}
+
+			PC->UpdateRotation(DeltaTime);
+		}
+	}
+	else
+	{
+		if (PC && bUseClientControlRotation)
+		{
+			PC->SetControlRotation(ViewRot);
+		}
 	}
 
 	UE_LOG(LogVRCharacterMovement, VeryVerbose, TEXT("ServerMove Time %f Acceleration %s Position %s DeltaTime %f"),
