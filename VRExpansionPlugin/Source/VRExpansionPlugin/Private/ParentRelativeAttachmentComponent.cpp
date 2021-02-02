a087403464fca803c93ce87cb1f497e49002b9f2
mordentral <morden@noxhub.net>
2021-02-01 20:01:58 -0500

    missed a change
    missed a change

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/ParentRelativeAttachmentComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/ParentRelativeAttachmentComponent.cpp
@@ -46,9 +46,9 @@ void UParentRelativeAttachmentComponent::InitializeComponent()
 	Super::InitializeComponent();
 
 	// Update our tracking
-	if (AttachChar.IsValid()) // New case to early out and with less calculations
+	if (!bUseFeetLocation && AttachChar.IsValid()) // New case to early out and with less calculations
 	{
-		SetRelativeTransform(AttachChar->VRReplicatedCamera->GetComponentTransform());
+		SetRelativeTransform(AttachChar->VRReplicatedCamera->GetRelativeTransform());
 	}
 
 }
