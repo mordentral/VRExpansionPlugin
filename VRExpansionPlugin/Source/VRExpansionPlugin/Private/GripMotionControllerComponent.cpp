c495e62db0ba1dcebfc523e309fbcca4fba89d1d
mordentral <morden@noxhub.net>
2021-03-06 00:09:32 -0500

    fixed teleport not clearing when holding a DropOnTeleport object
    fixed teleport not clearing when holding a DropOnTeleport object. Also cleaned up some teleport functions more.

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -350,7 +350,7 @@ void UGripMotionControllerComponent::EndPlay(const EEndPlayReason::Type EndPlayR
 		DestroyPhysicsHandle(GrippedObjects[i]);
 
 		if (/*HasGripAuthority(GrippedObjects[i]) || */IsServer())
-			DropObjectByInterface(GrippedObjects[i].GrippedObject);
+			DropObjectByInterface(nullptr, GrippedObjects[i].GripID);
 	}
 	GrippedObjects.Empty();
 
@@ -359,7 +359,7 @@ void UGripMotionControllerComponent::EndPlay(const EEndPlayReason::Type EndPlayR
 		DestroyPhysicsHandle(LocallyGrippedObjects[i]);
 
 		if (/*HasGripAuthority(LocallyGrippedObjects[i]) || */IsServer())
-			DropObjectByInterface(LocallyGrippedObjects[i].GrippedObject);
+			DropObjectByInterface(nullptr, LocallyGrippedObjects[i].GripID);
 	}
 	LocallyGrippedObjects.Empty();
 
@@ -3693,7 +3693,7 @@ bool UGripMotionControllerComponent::TeleportMoveGrip_Impl(FBPActorGripInformati
 				Grip.GripMovementReplicationSetting == EGripMovementReplicationSettings::ClientSide_Authoritive ||
 				Grip.GripMovementReplicationSetting == EGripMovementReplicationSettings::ClientSide_Authoritive_NoRep)
 			{
-				DropObjectByInterface(Grip.GrippedObject);
+				DropObjectByInterface(nullptr, Grip.GripID);
 			}
 			
 			return false; // Didn't teleport
@@ -4146,9 +4146,13 @@ void UGripMotionControllerComponent::TickGrip(float DeltaTime)
 	HandleGripArray(LocallyGrippedObjects, ParentTransform, DeltaTime);
 
 	// Empty out the teleport flag, checking original state just in case the player changed it while processing bps
-	if (bOriginalPostTeleport && (GrippedObjects.Num() || LocallyGrippedObjects.Num()))
+	if (bOriginalPostTeleport)
 	{
-		OnTeleportedGrips.Broadcast();
+		if ((GrippedObjects.Num() || LocallyGrippedObjects.Num()))
+		{
+			OnTeleportedGrips.Broadcast();
+		}
+
 		bIsPostTeleport = false;
 	}
 
@@ -6058,7 +6062,7 @@ void UGripMotionControllerComponent::Client_NotifyInvalidLocalGrip_Implementatio
 	{
 		if (FBPActorGripInformation* GripInfo = GetGripPtrByID(GripID))
 		{
-			DropObjectByInterface(GripInfo->GrippedObject, GripID);
+			DropObjectByInterface(nullptr, GripID);
 			return;
 		}		
 	}
@@ -6072,7 +6076,7 @@ void UGripMotionControllerComponent::Client_NotifyInvalidLocalGrip_Implementatio
 		return;
 
 	// Drop it, server told us that it was a bad grip
-	DropObjectByInterface(FoundGrip.GrippedObject, FoundGrip.GripID);
+	DropObjectByInterface(nullptr, FoundGrip.GripID);
 }
 
 bool UGripMotionControllerComponent::Server_NotifyHandledTransaction_Validate(uint8 GripID)
