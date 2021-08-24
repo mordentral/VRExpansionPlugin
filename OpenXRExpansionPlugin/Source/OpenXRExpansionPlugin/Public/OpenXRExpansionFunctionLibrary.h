11729b147db19fc2a44e8cad4ea6b31bd1a3e52f
Joshua <mordentral@gmail.com>
2021-08-24 14:52:27 -0400

    added log output for if we can't match the openXR interaction profile
    added log output for if we can't match the openXR interaction profile

--- OpenXRExpansionPlugin/Source/OpenXRExpansionPlugin/Public/OpenXRExpansionFunctionLibrary.h
+++ OpenXRExpansionPlugin/Source/OpenXRExpansionPlugin/Public/OpenXRExpansionFunctionLibrary.h
@@ -17,6 +17,9 @@
 
 #include "OpenXRExpansionFunctionLibrary.generated.h"
 
+
+DECLARE_LOG_CATEGORY_EXTERN(OpenXRExpansionFunctionLibraryLog, Log, All);
+
 // This needs to be updated as the original gets changed, that or hope they make the original blueprint accessible.
 UENUM(Blueprintable)
 enum class EBPOpenXRControllerDeviceType : uint8
@@ -58,7 +61,7 @@ public:
 	}
 
 	UFUNCTION(BlueprintCallable, Category = "VRExpansionFunctions|OpenXR", meta = (bIgnoreSelf = "true"))
-	static bool GetOpenXRHandPose(FBPOpenXRActionSkeletalData& HandPoseContainer, UOpenXRHandPoseComponent * HandPoseComponent, bool bGetMockUpPose = false)
+		static bool GetOpenXRHandPose(FBPOpenXRActionSkeletalData& HandPoseContainer, UOpenXRHandPoseComponent* HandPoseComponent, bool bGetMockUpPose = false)
 	{
 		FXRMotionControllerData MotionControllerData;
 
@@ -198,7 +201,7 @@ public:
 	}
 
 	//UFUNCTION(BlueprintCallable, Category = "VRExpansionFunctions|OpenXR", meta = (bIgnoreSelf = "true"))
-		static void GetMockUpControllerData(FXRMotionControllerData& MotionControllerData, FBPOpenXRActionSkeletalData& SkeletalMappingData, bool bOpenHand = false)
+	static void GetMockUpControllerData(FXRMotionControllerData& MotionControllerData, FBPOpenXRActionSkeletalData& SkeletalMappingData, bool bOpenHand = false)
 	{
 
 
@@ -293,7 +296,7 @@ public:
 			FVector(-1204.222f,-523.061f,277.211)
 		};
 
-			// Open Hand
+		// Open Hand
 		TArray<FVector> HandPositionsOpen = {
 			FVector(-1014.001f,-478.278f,212.902f),
 			FVector(-1013.516f,-476.006f,214.688f),
@@ -332,8 +335,8 @@ public:
 		}
 		else
 		{
-			MotionControllerData.GripPosition = FVector(-1202.619f,-521.077f,283.076f);
-			MotionControllerData.GripRotation = FQuat(0.040843058f,0.116659224f,0.980030060f,-0.155767411f);
+			MotionControllerData.GripPosition = FVector(-1202.619f, -521.077f, 283.076f);
+			MotionControllerData.GripRotation = FQuat(0.040843058f, 0.116659224f, 0.980030060f, -0.155767411f);
 		}
 
 		MotionControllerData.DeviceName = TEXT("OpenXR");
@@ -349,125 +352,5 @@ public:
 
 	// Get a list of all currently tracked devices and their types, index in the array is their device index
 	UFUNCTION(BlueprintPure, Category = "VRExpansionFunctions|OpenXR", meta = (bIgnoreSelf = "true"))
-		static bool GetXRMotionControllerType(FString &TrackingSystemName, EBPOpenXRControllerDeviceType &DeviceType)
-	{
-		DeviceType = EBPOpenXRControllerDeviceType::DT_UnknownController;
-
-		if (FOpenXRHMD* pOpenXRHMD = GetOpenXRHMD())
-		{
-			XrInstance XRInstance = pOpenXRHMD->GetInstance();
-			XrSystemId XRSysID = pOpenXRHMD->GetSystem();
-
-			if (XRSysID && XRInstance)
-			{
-				XrSystemProperties systemProperties{ XR_TYPE_SYSTEM_PROPERTIES };
-				systemProperties.next = nullptr;
-
-				if (xrGetSystemProperties(XRInstance, XRSysID, &systemProperties) == XR_SUCCESS)
-				{
-					XrSession XRSesh = pOpenXRHMD->GetSession();
-
-					if (XRSesh)
-					{
-						XrPath myPath;
-						XrResult PathResult = xrStringToPath(XRInstance, "/user/hand/left", &myPath);
-						XrInteractionProfileState interactionProfile{ XR_TYPE_INTERACTION_PROFILE_STATE };
-						interactionProfile.next = nullptr;
-						if (xrGetCurrentInteractionProfile(XRSesh, myPath, &interactionProfile) == XR_SUCCESS)
-						{
-							char myPathy[XR_MAX_SYSTEM_NAME_SIZE];
-							uint32_t outputsize;
-							xrPathToString(XRInstance, interactionProfile.interactionProfile, XR_MAX_SYSTEM_NAME_SIZE, &outputsize, myPathy);
-
-							if (outputsize < 1)
-								return false;
-
-							FString InteractionName(ANSI_TO_TCHAR(myPathy));
-
-							/*
-							* Interaction profile paths [6.4]
-							An interaction profile identifies a collection of buttons and
-							other input sources, and is of the form:
-							 /interaction_profiles/<vendor_name>/<type_name>
-							Paths supported in the core 1.0 release
-							 /interaction_profiles/khr/simple_control
-													 /interaction_profiles/khr/simple_controller
-							 /interaction_profiles/google/daydream_controller
-							 /interaction_profiles/htc/vive_controller
-							 /interaction_profiles/htc/vive_pro
-							 /interaction_profiles/microsoft/motion_controller
-							 /interaction_profiles/microsoft/xbox_controller
-							 /interaction_profiles/oculus/go_controller
-							 /interaction_profiles/oculus/touch_controller
-							 /interaction_profiles/valve/index_controller
-							*/
-
-							// Not working currently?
-							/*XrInputSourceLocalizedNameGetInfo InputSourceInfo{ XR_TYPE_INPUT_SOURCE_LOCALIZED_NAME_GET_INFO };
-							InputSourceInfo.next = nullptr;
-							InputSourceInfo.whichComponents = XR_INPUT_SOURCE_LOCALIZED_NAME_INTERACTION_PROFILE_BIT;
-							InputSourceInfo.sourcePath = interactionProfile.interactionProfile;
-
-							char buffer[XR_MAX_SYSTEM_NAME_SIZE];
-							uint32_t usedBufferCount = 0;
-							if (xrGetInputSourceLocalizedName(XRSesh, &InputSourceInfo, XR_MAX_SYSTEM_NAME_SIZE, &usedBufferCount, (char*)&buffer) == XR_SUCCESS)
-							{
-								int g = 0;
-							}*/
-
-
-							if (InteractionName.Find("touch_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_OculusTouchController;
-							}
-							else if (InteractionName.Contains("index_controller", ESearchCase::IgnoreCase))
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_ValveIndexController;
-							}
-							else if (InteractionName.Find("vive_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_ViveController;
-							}
-							else if (InteractionName.Find("vive_pro", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_ViveProController;
-							}
-							else if (InteractionName.Find("simple_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_SimpleController;
-							}
-							else if (InteractionName.Find("daydream_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_DaydreamController;
-							}
-							else if (InteractionName.Find("motion_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_MicrosoftMotionController;
-							}
-							else if (InteractionName.Find("xbox_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_MicrosoftXboxController;
-							}
-							else if (InteractionName.Find("go_controller", ESearchCase::IgnoreCase) != INDEX_NONE)
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_OculusGoController;
-							}
-							else
-							{
-								DeviceType = EBPOpenXRControllerDeviceType::DT_UnknownController;		
-							}
-						}
-
-						TrackingSystemName = FString(ANSI_TO_TCHAR(systemProperties.systemName));// , XR_MAX_SYSTEM_NAME_SIZE);
-						//VendorID = systemProperties.vendorId;
-						return true;
-					}
-				}
-			}
-		}
-
-		TrackingSystemName.Empty();
-		return false;
-	}
-
-};	
+		static bool GetXRMotionControllerType(FString& TrackingSystemName, EBPOpenXRControllerDeviceType& DeviceType);
+};
