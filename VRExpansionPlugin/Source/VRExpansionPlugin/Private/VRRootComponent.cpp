9ba963f6158790cf0fe039336a837737a5eba1de
mordentral <morden@noxhub.net>
2021-09-11 15:47:51 -0400

    didnt need to update offset here, waste of perf
    didnt need to update offset here, waste of perf

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRRootComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRRootComponent.cpp
@@ -1487,7 +1487,7 @@ bool UVRRootComponent::ConvertSweptOverlapsToCurrentOverlaps(
 	checkSlow(SweptOverlapsIndex >= 0);
 
 	// Override location check with our own
-	GenerateOffsetToWorld();
+	//GenerateOffsetToWorld();
 	FVector EndLocationVR = OffsetComponentToWorld.GetLocation();
 
 
