89a1db78ee477c3631a1aa6ad1299420402bf9ed
mordentral <morden@noxhub.net>
2021-09-23 20:39:22 -0400

    making the interactibles obey the motion controllers OnGripOutOfRange event
    making the interactibles obey the motion controllers OnGripOutOfRange event

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/Interactibles/VRSliderComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/Interactibles/VRSliderComponent.cpp
@@ -423,7 +423,15 @@ bool UVRSliderComponent::CheckAutoDrop(UGripMotionControllerComponent * Gripping
 	// Converted to a relative value now so it should be correct
 	if (BreakDistance > 0.f && GrippingController->HasGripAuthority(GripInformation) && FVector::DistSquared(InitialDropLocation, this->GetComponentTransform().InverseTransformPosition(GrippingController->GetPivotLocation())) >= FMath::Square(BreakDistance))
 	{
-		GrippingController->DropObjectByInterface(this, HoldingGrip.GripID);
+		if (GrippingController->OnGripOutOfRange.IsBound())
+		{
+			uint8 GripID = GripInformation.GripID;
+			GrippingController->OnGripOutOfRange.Broadcast(GripInformation, GripInformation.GripDistance);
+		}
+		else
+		{
+			GrippingController->DropObjectByInterface(this, HoldingGrip.GripID);
+		}
 		return true;
 	}
 
