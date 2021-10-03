8298aad50dce8068650dc699a5cbbb8c5100eb74
mordentral <morden@noxhub.net>
2021-10-03 19:51:41 -0400

    default euro smoothing when enabled
    default euro smoothing when enabled

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -107,7 +107,7 @@ UGripMotionControllerComponent::UGripMotionControllerComponent(const FObjectInit
 
 	bSmoothHandTracking = false;
 	bWasSmoothingHand = false;
-	bSmoothWithEuroLowPassFunction = false;
+	bSmoothWithEuroLowPassFunction = true;
 	LastSmoothRelativeTransform = FTransform::Identity;
 	SmoothingSpeed = 20.0f;
 	EuroSmoothingParams.MinCutoff = 0.1f;
