04db6334c2b2c65db4f459fefd63bd8f54d6139a
Joshua <mordentral@gmail.com>
2021-11-23 10:24:22 -0500

    added hand scale option to hand socket for people not using absolute scale
    added hand scale option to hand socket for people not using absolute scale, also make the first value of the vr camera components velocity skip the first calc since the initial is invalid

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
@@ -69,6 +69,7 @@ UReplicatedVRCameraComponent::UReplicatedVRCameraComponent(const FObjectInitiali
 
 	LastRelativePosition = FTransform::Identity;
 	bSampleVelocityInWorldSpace = false;
+	bHadValidFirstVelocity = false;
 
 	//bUseVRNeckOffset = true;
 	//VRNeckOffset = FTransform(FRotator::ZeroRotator, FVector(15.0f,0,0), FVector(1.0f));
@@ -213,7 +214,12 @@ void UReplicatedVRCameraComponent::UpdateTracking(float DeltaTime)
 	}
 
 	// Save out the component velocity from this and last frame
-	ComponentVelocity = ((bSampleVelocityInWorldSpace ? GetComponentLocation() : GetRelativeLocation()) - LastRelativePosition.GetTranslation()) / DeltaTime;
+	if(bHadValidFirstVelocity || !LastRelativePosition.Equals(FTransform::Identity))
+	{ 
+		bHadValidFirstVelocity = true;
+		ComponentVelocity = ((bSampleVelocityInWorldSpace ? GetComponentLocation() : GetRelativeLocation()) - LastRelativePosition.GetTranslation()) / DeltaTime;
+	}
+
 	LastRelativePosition = bSampleVelocityInWorldSpace ? this->GetComponentTransform() : this->GetRelativeTransform();
 }
 
