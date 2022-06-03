cc469fc39c2116cdb395cf375ffe1367f70af34b
Joshua <mordentral@gmail.com>
2022-06-03 12:12:03 -0400

    some edge case handling for if someone is dropping our grip out from under a grip

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -1553,10 +1553,15 @@ bool UGripMotionControllerComponent::GripActor(
 				}
 			}
 
-			FBPActorGripInformation GripInfo = LocallyGrippedObjects[Index];
-
-			if (GetNetMode() == ENetMode::NM_Client && !IsTornOff() && GripInfo.GripMovementReplicationSetting == EGripMovementReplicationSettings::ClientSide_Authoritive)
-				Server_NotifyLocalGripAddedOrChanged(GripInfo);
+			if (bIsLocalGrip && GetNetMode() == ENetMode::NM_Client && !IsTornOff())
+			{
+				Index = LocallyGrippedObjects.IndexOfByKey(newActorGrip.GripID);
+				if (Index != INDEX_NONE)
+				{
+					FBPActorGripInformation GripInfo = LocallyGrippedObjects[Index];
+					Server_NotifyLocalGripAddedOrChanged(GripInfo);
+				}
+			}
 		}
 	}
 	//NotifyGrip(newActorGrip);
@@ -1802,10 +1807,15 @@ bool UGripMotionControllerComponent::GripComponent(
 				}
 			}
 
-			FBPActorGripInformation GripInfo = LocallyGrippedObjects[Index];
-
-			if (GetNetMode() == ENetMode::NM_Client && !IsTornOff() && GripInfo.GripMovementReplicationSetting == EGripMovementReplicationSettings::ClientSide_Authoritive)
-				Server_NotifyLocalGripAddedOrChanged(GripInfo);
+			if (bIsLocalGrip && GetNetMode() == ENetMode::NM_Client && !IsTornOff())
+			{
+				Index = LocallyGrippedObjects.IndexOfByKey(newComponentGrip.GripID);
+				if (Index != INDEX_NONE)
+				{
+					FBPActorGripInformation GripInfo = LocallyGrippedObjects[Index];
+					Server_NotifyLocalGripAddedOrChanged(GripInfo);
+				}
+			}
 		}
 	}
 
@@ -2888,6 +2898,10 @@ bool UGripMotionControllerComponent::NotifyGrip(FBPActorGripInformation &NewGrip
 	{
 		// Broadcast a new grip
 		OnGrippedObject.Broadcast(NewGrip);
+		if (!LocallyGrippedObjects.Contains(NewGrip.GripID) && !GrippedObjects.Contains(NewGrip.GripID))
+		{
+			return false;
+		}
 	}
 
 	return true;
