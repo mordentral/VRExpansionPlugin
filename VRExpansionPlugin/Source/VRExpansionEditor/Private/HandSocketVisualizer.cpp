136f9449f1bf0802be8f35cda60ccc7a047a852f
Joshua <mordentral@gmail.com>
2022-07-21 09:41:49 -0400

    merging push

--- VRExpansionPlugin/Source/VRExpansionEditor/Private/HandSocketVisualizer.cpp
+++ VRExpansionPlugin/Source/VRExpansionEditor/Private/HandSocketVisualizer.cpp
@@ -6,6 +6,7 @@
 #include "SceneManagement.h"
 //#include "UObject/Field.h"
 #include "VRBPDatatypes.h"
+#include "ScopedTransaction.h"
 #include "Modules/ModuleManager.h"
 #include "EditorViewportClient.h"
 #include "Components/PoseableMeshComponent.h"
