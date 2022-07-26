9f09bcc5e8a241cf8f1e99919050e3a512d5a15e
Joshua <mordentral@gmail.com>
2022-07-26 09:57:53 -0400

    scale controller hmd sample by controller tracking scale

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -6412,6 +6412,12 @@ bool UGripMotionControllerComponent::GripPollControllerState(FVector& Position, 
 							{
 								//curLoc.Z = 0;
 								LastLocationForLateUpdate = curLoc;
+
+								// Assume HMD has the same tracking scaler
+								if (bScaleTracking)
+								{
+									LastLocationForLateUpdate *= TrackingScaler;
+								}
 							}
 							else
 							{
