a0850795d982478332a4fc1dd1749b17ae037712
Joshua <mordentral@gmail.com>
2022-10-11 13:16:29 -0400

    port velocity setting changes

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/GripMotionControllerComponent.cpp
@@ -5685,9 +5685,12 @@ bool UGripMotionControllerComponent::DestroyPhysicsHandle(const FBPActorGripInfo
 					rBodyInstance->UpdateMassProperties();
 				}
 
-				// Offset the linear velocity by the new COM position and set it
-				vel += FVector::CrossProduct(aVel, rBodyInstance->GetCOMPosition() - originalCOM);
-				rBodyInstance->SetLinearVelocity(vel, false);
+				if (rBodyInstance->IsInstanceSimulatingPhysics())
+				{
+					// Offset the linear velocity by the new COM position and set it
+					vel += FVector::CrossProduct(aVel, rBodyInstance->GetCOMPosition() - originalCOM);
+					rBodyInstance->SetLinearVelocity(vel, false);
+				}
 			}
 		}
 	}
