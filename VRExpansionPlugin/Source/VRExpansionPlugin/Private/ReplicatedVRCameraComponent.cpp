9bd53721e16c23bf391c10d25e6be92684a87650
Joshua <mordentral@gmail.com>
2022-10-11 12:51:45 -0400

    more fixes

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
@@ -297,7 +297,7 @@ void UReplicatedVRCameraComponent::TickComponent(float DeltaTime, enum ELevelTic
 	}
 }
 
-void UReplicatedVRCameraComponent::GetCameraView(float DeltaTime, FMinimalViewInfo& DesiredView)
+void UReplicatedVRCameraComponent::HandleXRCamera()
 {
 	bool bIsLocallyControlled = IsLocallyControlled();
 
@@ -320,7 +320,7 @@ void UReplicatedVRCameraComponent::GetCameraView(float DeltaTime, FMinimalViewIn
 			{
 				const FTransform ParentWorld = CalcNewComponentToWorld(FTransform());
 				XRCamera->SetupLateUpdate(ParentWorld, this, bLockToHmd == 0);
-				
+
 				if (bLockToHmd)
 				{
 					FQuat Orientation;
@@ -348,52 +348,4 @@ void UReplicatedVRCameraComponent::GetCameraView(float DeltaTime, FMinimalViewIn
 			}
 		}
 	}
-
-	if (bUsePawnControlRotation)
-	{
-		const APawn* OwningPawn = Cast<APawn>(GetOwner());
-		const AController* OwningController = OwningPawn ? OwningPawn->GetController() : nullptr;
-		if (OwningController && OwningController->IsLocalPlayerController())
-		{
-			const FRotator PawnViewRotation = OwningPawn->GetViewRotation();
-			if (!PawnViewRotation.Equals(GetComponentRotation()))
-			{
-				SetWorldRotation(PawnViewRotation);
-			}
-		}
-	}
-
-	if (bUseAdditiveOffset)
-	{
-		FTransform OffsetCamToBaseCam = AdditiveOffset;
-		FTransform BaseCamToWorld = GetComponentToWorld();
-		FTransform OffsetCamToWorld = OffsetCamToBaseCam * BaseCamToWorld;
-
-		DesiredView.Location = OffsetCamToWorld.GetLocation();
-		DesiredView.Rotation = OffsetCamToWorld.Rotator();
-	}
-	else
-	{
-		DesiredView.Location = GetComponentLocation();
-		DesiredView.Rotation = GetComponentRotation();
-	}
-
-	DesiredView.FOV = bUseAdditiveOffset ? (FieldOfView + AdditiveFOVOffset) : FieldOfView;
-	DesiredView.AspectRatio = AspectRatio;
-	DesiredView.bConstrainAspectRatio = bConstrainAspectRatio;
-	DesiredView.bUseFieldOfViewForLOD = bUseFieldOfViewForLOD;
-	DesiredView.ProjectionMode = ProjectionMode;
-	DesiredView.OrthoWidth = OrthoWidth;
-	DesiredView.OrthoNearClipPlane = OrthoNearClipPlane;
-	DesiredView.OrthoFarClipPlane = OrthoFarClipPlane;
-
-	// See if the CameraActor wants to override the PostProcess settings used.
-	DesiredView.PostProcessBlendWeight = PostProcessBlendWeight;
-	if (PostProcessBlendWeight > 0.0f)
-	{
-		DesiredView.PostProcessSettings = PostProcessSettings;
-	}
-
-	// If this camera component has a motion vector simumlation transform, use that for the current view's previous transform
-	DesiredView.PreviousViewTransform = FMotionVectorSimulation::Get().GetPreviousTransform(this);
 }
