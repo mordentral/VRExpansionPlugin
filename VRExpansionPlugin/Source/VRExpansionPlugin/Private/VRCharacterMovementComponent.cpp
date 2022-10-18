29efdef1b68e9d387fa80655f4594210fc238f0d
Joshua <mordentral@gmail.com>
2022-10-18 11:28:36 -0400

    preview 2 update

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRCharacterMovementComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/VRCharacterMovementComponent.cpp
@@ -1147,102 +1147,6 @@ void UVRCharacterMovementComponent::CapsuleTouched(UPrimitiveComponent* Overlapp
 	}
 }
 
-void UVRCharacterMovementComponent::ApplyImpactPhysicsForces(const FHitResult& Impact, const FVector& ImpactAcceleration, const FVector& ImpactVelocity)
-{
- 	if (bEnablePhysicsInteraction && Impact.bBlockingHit)
-	{
-
-		static const auto CVarbGeometryCollectionImpulseWorkAround = IConsoleManager::Get().FindConsoleVariable(TEXT("p.CVarGeometryCollectionImpulseWorkAround"));
-
-		if (UPrimitiveComponent* ImpactComponent = Impact.GetComponent())
-		{
-			FVector ForcePoint = Impact.ImpactPoint;
-			float BodyMass = 1.0f; // set to 1 as this is used as a multiplier
-
-			bool bCanBePushed = false;
-			FBodyInstance* BI = ImpactComponent->GetBodyInstance(Impact.BoneName);
-			if (BI != nullptr && BI->IsInstanceSimulatingPhysics())
-			{
-				bCanBePushed = true;
-				BodyMass = FMath::Max(BI->GetBodyMass(), 1.0f);
-
-				if (bPushForceUsingZOffset)
-				{
-					FBox Bounds = BI->GetBodyBounds();
-
-					FVector Center, Extents;
-					Bounds.GetCenterAndExtents(Center, Extents);
-
-					if (!Extents.IsNearlyZero())
-					{
-						ForcePoint.Z = Center.Z + Extents.Z * PushForcePointZOffsetFactor;
-					}
-				}
-			}
-			else if (CVarbGeometryCollectionImpulseWorkAround->GetBool())
-			{
-				const FName ClassName = ImpactComponent->GetClass()->GetFName();
-				const FName GeometryCollectionClassName("UGeometryCollectionComponent");
-				if (ClassName == GeometryCollectionClassName && ImpactComponent->BodyInstance.bSimulatePhysics)
-				{
-					// in some case GetBodyInstance can return null while the BodyInstance still exists ( geometry collection component for example )
-					// but we cannot check for its component directly here because of modules cyclic dependencies
-					// todo(chaos): change this logic to be more driven at the primitive component level to avoid the high level classes to have to be aware of the different component
-
-					// because of the above limititation we have to ignore bPushForceUsingZOffset
-
-					bCanBePushed = true; // not necessary as this is already set by default but it's better than leaving this block empty
-				}
-			}
-			else
-			{
-				// THIS IS NEVER REACH IN 5.1 as the above isalways true with the CVAR!!! engine bug
-				// no body instance, not a GC, not supported scenario
-				bCanBePushed = false;
-			}
-			if (bCanBePushed)
-			{
-				FVector Force = Impact.ImpactNormal * -1.0f;
-
-				float PushForceModificator = 1.0f;
-
-				const FVector ComponentVelocity = ImpactComponent->GetPhysicsLinearVelocity();
-				const FVector VirtualVelocity = ImpactAcceleration.IsZero() ? ImpactVelocity : ImpactAcceleration.GetSafeNormal() * GetMaxSpeed();
-
-				float Dot = 0.0f;
-
-				if (bScalePushForceToVelocity && !ComponentVelocity.IsNearlyZero())
-				{
-					Dot = ComponentVelocity | VirtualVelocity;
-
-					if (Dot > 0.0f && Dot < 1.0f)
-					{
-						PushForceModificator *= Dot;
-					}
-				}
-
-				if (bPushForceScaledToMass)
-				{
-					PushForceModificator *= BodyMass;
-				}
-
-				Force *= PushForceModificator;
-				const float ZeroVelocityTolerance = 1.0f;
-				if (ComponentVelocity.IsNearlyZero(ZeroVelocityTolerance))
-				{
-					Force *= InitialPushForceFactor;
-					ImpactComponent->AddImpulseAtLocation(Force, ForcePoint, Impact.BoneName);
-				}
-				else
-				{
-					Force *= PushForceFactor;
-					ImpactComponent->AddForceAtLocation(Force, ForcePoint, Impact.BoneName);
-				}
-			}
-		}
-	}
-}
-
 void UVRCharacterMovementComponent::ReplicateMoveToServer(float DeltaTime, const FVector& NewAcceleration)
 {
 	SCOPE_CYCLE_COUNTER(STAT_CharacterMovementReplicateMoveToServer);
@@ -3932,10 +3836,7 @@ void UVRCharacterMovementComponent::ClientAdjustPositionVR_Implementation
 	//  Received Location is relative to dynamic base
 	if (bBaseRelativePosition)
 	{
-		FVector BaseLocation;
-		FQuat BaseRotation;
-		MovementBaseUtility::GetMovementBaseTransform(NewBase, NewBaseBoneName, BaseLocation, BaseRotation); // TODO: error handling if returns false		
-		WorldShiftedNewLocation = NewLocation + BaseLocation;
+		MovementBaseUtility::GetLocalMovementBaseLocationInWorldSpace(NewBase, NewBaseBoneName, NewLocation, WorldShiftedNewLocation); // TODO: error handling if returns false	
 	}
 	else
 	{
@@ -4093,10 +3994,7 @@ void UVRCharacterMovementComponent::ServerMoveHandleClientErrorVR(float ClientTi
 	FVector ClientLoc = RelativeClientLoc;
 	if (MovementBaseUtility::UseRelativeLocation(ClientMovementBase))
 	{
-		FVector BaseLocation;
-		FQuat BaseRotation;
-		MovementBaseUtility::GetMovementBaseTransform(ClientMovementBase, ClientBaseBoneName, BaseLocation, BaseRotation);
-		ClientLoc += BaseLocation;
+		MovementBaseUtility::GetLocalMovementBaseLocationInWorldSpace(ClientMovementBase, ClientBaseBoneName, RelativeClientLoc, ClientLoc);
 	}
 	else
 	{
@@ -4205,12 +4103,8 @@ void UVRCharacterMovementComponent::ServerMoveHandleClientErrorVR(float ClientTi
 				// To improve position syncing, use old base for take-off
 				if (MovementBaseUtility::UseRelativeLocation(LastServerMovementBaseVRPtr))
 				{
-					FVector BaseLocation;
-					FQuat BaseQuat;
-					MovementBaseUtility::GetMovementBaseTransform(LastServerMovementBaseVRPtr, LastServerMovementBaseBoneName, BaseLocation, BaseQuat);
-
 					// Relative Location
-					RelativeLocation = UpdatedComponent->GetComponentLocation() - BaseLocation;
+					MovementBaseUtility::GetLocalMovementBaseLocation(LastServerMovementBaseVRPtr, LastServerMovementBaseBoneName, UpdatedComponent->GetComponentLocation(), RelativeLocation);
 					bUseLastBase = true;
 				}
 			}
