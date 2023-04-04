210eb3441f2e2e81f4cb45327b611535364268f9
Joshua <mordentral@gmail.com>
2023-04-04 10:23:08 -0400

    Porting animation fixing tool to 4.27

---
+++ VRExpansionPlugin/Source/VRExpansionEditor/Private/VRGlobalSettingsDetails.cpp
@@ -0,0 +1,143 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VRGlobalSettingsDetails.h"
+#include "VRGlobalSettings.h"
+//#include "PropertyEditing.h"
+#include "Widgets/Text/STextBlock.h"
+#include "Widgets/Input/SButton.h"
+//#include "PropertyHandle.h"
+#include "DetailLayoutBuilder.h"
+#include "DetailWidgetRow.h"
+#include "DetailCategoryBuilder.h"
+#include "IDetailsView.h"
+
+//#include "Developer/AssetTools/Public/IAssetTools.h"
+//#include "Developer/AssetTools/Public/AssetToolsModule.h"
+//#include "Editor/ContentBrowser/Public/IContentBrowserSingleton.h"
+//#include "Editor/ContentBrowser/Public/ContentBrowserModule.h"
+//#include "AnimationUtils.h"
+
+#include "UObject/SavePackage.h"
+#include "Misc/MessageDialog.h"
+//#include "Widgets/Layout/SBorder.h"
+//#include "Widgets/Layout/SSeparator.h"
+//#include "Widgets/Layout/SUniformGridPanel.h"
+//#include "Widgets/Input/SEditableTextBox.h"
+#include "Editor.h"
+#include "EditorStyleSet.h"
+#include "Styling/CoreStyle.h"
+
+//#include "Animation/AnimData/AnimDataModel.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "AssetRegistry/ARFilter.h"
+
+#include "Editor/UnrealEdEngine.h"
+#include "UnrealEdGlobals.h"
+
+#define LOCTEXT_NAMESPACE "VRGlobalSettingsDetails"
+
+
+TSharedRef< IDetailCustomization > FVRGlobalSettingsDetails::MakeInstance()
+{
+    return MakeShareable(new FVRGlobalSettingsDetails);
+}
+
+void FVRGlobalSettingsDetails::CustomizeDetails(IDetailLayoutBuilder& DetailBuilder)
+{
+	//DetailBuilder.HideCategory(FName("ComponentTick"));
+	//DetailBuilder.HideCategory(FName("GameplayTags"));
+
+
+	//TSharedPtr<IPropertyHandle> LockedLocationProperty = DetailBuilder.GetProperty(GET_MEMBER_NAME_CHECKED(UHandSocketComponent, bDecoupleMeshPlacement));
+
+	//FSimpleDelegate OnLockedStateChangedDelegate = FSimpleDelegate::CreateSP(this, &FVRGlobalSettingsDetails::OnLockedStateUpdated, &DetailBuilder);
+	//LockedLocationProperty->SetOnPropertyValueChanged(OnLockedStateChangedDelegate);
+
+	DetailBuilder.EditCategory("Utilities")
+		.AddCustomRow(NSLOCTEXT("VRGlobalSettingsDetails", "Tools", "Fix Invalid 5.2 Animation Assets"))
+		.NameContent()
+		[
+			SNew(STextBlock)
+			.Font(IDetailLayoutBuilder::GetDetailFont())
+		.Text(NSLOCTEXT("VRGlobalSettingsDetails", "Tools", "Fix Invalid 5.2 Animation Assets"))
+		]
+	.ValueContent()
+		.MaxDesiredWidth(125.f)
+		.MinDesiredWidth(125.f)
+		[
+			SNew(SButton)
+			.ContentPadding(2)
+		.VAlign(VAlign_Center)
+		.HAlign(HAlign_Center)
+		.OnClicked(this, &FVRGlobalSettingsDetails::OnCorrectInvalidAnimationAssets)
+		[
+			SNew(STextBlock)
+			.Font(IDetailLayoutBuilder::GetDetailFont())
+		.Text(NSLOCTEXT("VRGlobalSettingsDetails", "Tools", "Fix Animation Assets"))
+		]
+		];
+}
+
+FReply FVRGlobalSettingsDetails::OnCorrectInvalidAnimationAssets()
+{
+
+	// Load the asset registry module
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked< FAssetRegistryModule >(FName("AssetRegistry"));
+	IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
+	TArray< FString > ContentPaths;
+	ContentPaths.Add(TEXT("/Game"));
+	AssetRegistry.ScanPathsSynchronous(ContentPaths);
+
+	FARFilter Filter;
+	Filter.ClassNames.Add(UAnimSequence::StaticClass()->GetFName());
+	Filter.bRecursiveClasses = true;
+	//if (!Path.IsEmpty())
+	//{
+	//	Filter.PackagePaths.Add(*Path);
+	//}
+	Filter.bRecursivePaths = true;
+
+	TArray< FAssetData > AssetList;
+	AssetRegistry.GetAssets(Filter, AssetList);
+
+	// Iterate over retrieved blueprint assets
+	for (auto& Asset : AssetList)
+	{
+		// Check the anim sequence for invalid data
+		if (UAnimSequence* AnimSeq = Cast<UAnimSequence>(Asset.GetAsset()))
+		{
+			{		
+				float FrameRateD = AnimSeq->GetFrameRate();
+
+				// I was saving with a below 1.0 frame rate and 1 frame
+				if (FrameRateD < 1.0f)
+				{
+
+					// We have an invalid frame rate for 5.2
+					AnimSeq->SequenceLength = 1.f;
+					AnimSeq->SetRawNumberOfFrame(1);
+					AnimSeq->MarkPackageDirty();
+
+					UPackage* const Package = AnimSeq->GetOutermost();
+					FString const PackageName = Package->GetName();
+					FString const PackageFileName = FPackageName::LongPackageNameToFilename(PackageName, FPackageName::GetAssetPackageExtension());
+
+					double StartTime = FPlatformTime::Seconds();
+
+					UPackage::SavePackage(Package, NULL, RF_Standalone, *PackageFileName, GError, nullptr, false, true, SAVE_NoError);
+
+					double ElapsedTime = FPlatformTime::Seconds() - StartTime;
+					UE_LOG(LogAnimation, Log, TEXT("Animation re-saved %s in %0.2f seconds"), *PackageName, ElapsedTime);
+				}
+	
+			}
+		}
+	}
+
+	UE_LOG(LogAnimation, Log, TEXT("Finished fixing animation assets!"));
+
+	return FReply::Handled();
+}
+
+
+#undef LOCTEXT_NAMESPACE
