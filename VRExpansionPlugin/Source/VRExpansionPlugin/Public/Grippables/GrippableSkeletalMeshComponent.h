c26591bee67eb47debdfd766f4ffda938c47f3e2
Joshua <mordentral@gmail.com>
2023-09-27 10:41:03 -0400

    Porting in skeletal mesh welding fixes to my classes as epic still hasn't fixed it yet.
    Porting in skeletal mesh welding fixes to my classes as epic still hasn't fixed it yet. Hopefully these can be removed at some point.

--- VRExpansionPlugin/Source/VRExpansionPlugin/Public/Grippables/GrippableSkeletalMeshComponent.h
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Public/Grippables/GrippableSkeletalMeshComponent.h
@@ -31,6 +31,10 @@ public:
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+	// Weld Fix until epic fixes it #TODO: check back in on this and remove and the asset include when epic fixes it
+	virtual void GetWeldedBodies(TArray<FBodyInstance*>& OutWeldedBodies, TArray<FName>& OutLabels, bool bIncludingAutoWeld) override;
+	virtual FBodyInstance* GetBodyInstance(FName BoneName = NAME_None, bool bGetWelded = true, int32 Index = INDEX_NONE) const override;
+
 	UPROPERTY(EditAnywhere, Replicated, BlueprintReadOnly, Instanced, Category = "VRGripInterface")
 		TArray<TObjectPtr<UVRGripScriptBase>> GripLogicScripts;
 
