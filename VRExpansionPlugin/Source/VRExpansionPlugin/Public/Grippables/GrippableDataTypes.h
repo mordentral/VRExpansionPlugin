78a70116b01ed3d9c6527acac017089955b52d62
Ronald Burns <7757589+ronaldburns@users.noreply.github.com>
2023-09-29 17:20:38 -0500

    Expose FRepAttachmentWithWeld

--- VRExpansionPlugin/Source/VRExpansionPlugin/Public/Grippables/GrippableDataTypes.h
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Public/Grippables/GrippableDataTypes.h
@@ -8,7 +8,7 @@
 
 // A version of the attachment structure that include welding data
 USTRUCT()
-struct FRepAttachmentWithWeld : public FRepAttachment
+struct VREXPANSIONPLUGIN_API FRepAttachmentWithWeld : public FRepAttachment
 {
 public:
 	GENERATED_BODY()
