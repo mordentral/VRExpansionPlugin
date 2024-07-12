07f3b94902d331ac68dc3b5e56d097efe9e6f7da
Joshua <mordentral@gmail.com>
2024-07-12 15:03:43 -0400

    Revise skipping replication to handle case where owner hasn't replicated yet.
    Revise skipping replication to handle case where owner hasn't replicated yet. Used to not really be an issue but the engine in UE5 is now re-applying the physics replication addition.

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/Grippables/GrippableActor.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/Grippables/GrippableActor.cpp
@@ -254,6 +254,30 @@ void AGrippableActor::GatherCurrentMovement()
 	}
 }
 
+bool AGrippableActor::ShouldWeSkipAttachmentReplication(bool bConsiderHeld) const
+{
+	if ((bConsiderHeld && !VRGripInterfaceSettings.bWasHeld) || GetNetMode() < ENetMode::NM_Client)
+		return false;
+
+	if (VRGripInterfaceSettings.MovementReplicationType == EGripMovementReplicationSettings::ClientSide_Authoritive ||
+		VRGripInterfaceSettings.MovementReplicationType == EGripMovementReplicationSettings::ClientSide_Authoritive_NoRep)
+	{
+		// First return if we are locally held (owner may not have replicated yet)
+		for (const FBPGripPair& Grip : VRGripInterfaceSettings.HoldingControllers)
+		{
+			if (IsValid(Grip.HoldingController) && Grip.HoldingController->IsLocallyControlled())
+			{
+				return true;
+			}
+		}
+
+		// then return if we have a local net owner
+		return HasLocalNetOwner();
+	}
+	else
+		return false;
+}
+
 void AGrippableActor::OnRep_AttachmentReplication()
 {
 	if (bAllowIgnoringAttachOnOwner && (ClientAuthReplicationData.bIsCurrentlyClientAuth || ShouldWeSkipAttachmentReplication()))
