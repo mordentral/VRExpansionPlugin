65a5c0543f2764db5bbba5bc4bffdf9a831c854e
Joshua (MordenTral) Statzer <morden@noxhub.net>
2024-09-30 12:16:45 -0400

    Merge pull request #54 from JunjieXian/5.3-Locked
    Fixed undefined type 'ESkinCacheUsage' when compiling on ue5.3

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/Grippables/HandSocketComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/Grippables/HandSocketComponent.cpp
@@ -19,6 +19,7 @@
 #include "GripMotionControllerComponent.h"
 //#include "VRGripInterface.h"
 //#include "VRBPDatatypes.h"
+#include "Engine/SkinnedAssetCommon.h"
 #include "Net/UnrealNetwork.h"
 #include "Serialization/CustomVersion.h"
 
