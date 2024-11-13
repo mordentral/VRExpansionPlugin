2074d4de4d73eb860f17969dbbcb63c744d991a7
Joshua <mordentral@gmail.com>
2024-11-13 12:40:47 -0500

    Adding Getting / settign final gear ratio per request

--- VRExpansionPlugin/Source/VRExpansionPlugin/Public/Misc/VRWheeledVehicle.h
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Public/Misc/VRWheeledVehicle.h
@@ -13,6 +13,40 @@
 #include "VRWheeledVehicle.generated.h"
 
 
+// This only exists to expose the blueprint properties of the transmission values
+USTRUCT(BlueprintType, Category = "VRWheeledVehicle")
+struct VREXPANSIONPLUGIN_API FBPVehicleTransmissionConfig : public FVehicleTransmissionConfig
+{
+	GENERATED_BODY()
+public:
+
+	void SetFrom(FVehicleTransmissionConfig & Config)
+	{
+		bUseAutomaticGears = Config.bUseAutomaticGears;
+		bUseAutoReverse = Config.bUseAutoReverse;
+		FinalRatio = Config.FinalRatio;
+		ForwardGearRatios = Config.ForwardGearRatios;
+		ReverseGearRatios = Config.ReverseGearRatios;
+		ChangeUpRPM = Config.ChangeUpRPM;
+		ChangeDownRPM = Config.ChangeDownRPM;
+		GearChangeTime = Config.GearChangeTime;
+		TransmissionEfficiency = Config.TransmissionEfficiency;
+	}
+
+	void SetTo(FVehicleTransmissionConfig& Config)
+	{
+		Config.bUseAutomaticGears = bUseAutomaticGears;
+		Config.bUseAutoReverse = bUseAutoReverse;
+		Config.FinalRatio = FinalRatio;
+		Config.ForwardGearRatios = ForwardGearRatios;
+		Config.ReverseGearRatios = ReverseGearRatios;
+		Config.ChangeUpRPM = ChangeUpRPM;
+		Config.ChangeDownRPM = ChangeDownRPM;
+		Config.GearChangeTime = GearChangeTime;
+		Config.TransmissionEfficiency = TransmissionEfficiency;
+	}
+};
+
 /**
 * This override of the base wheeled vehicle allows for dual pawn usage in engine.
 */
@@ -24,6 +58,29 @@ class VREXPANSIONPLUGIN_API AVRWheeledVehicle : public AWheeledVehiclePawn
 
 public:
 
+
+	UFUNCTION(BlueprintPure, Category = "Pawn")
+	float GetFinalGearRatio()
+	{
+		float CurrentGearRatio = 0.0f;
+		if (UChaosWheeledVehicleMovementComponent* MoveComp = Cast<UChaosWheeledVehicleMovementComponent>(this->GetMovementComponent()))
+		{		
+			CurrentGearRatio = MoveComp->TransmissionSetup.FinalRatio;
+		}
+
+		return CurrentGearRatio;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Pawn")
+	void SetFinalGearRatio(float NewFinalGearRatio)
+	{
+		if (UChaosWheeledVehicleMovementComponent* MoveComp = Cast<UChaosWheeledVehicleMovementComponent>(this->GetMovementComponent()))
+		{
+			MoveComp->TransmissionSetup.FinalRatio = NewFinalGearRatio;
+		}
+	}
+
+	// Calls the movement components override controller function
 	UFUNCTION(BlueprintCallable, Category = "Pawn")
 		virtual bool SetBindToInput(AController * CController, bool bBindToInput)
 	{
