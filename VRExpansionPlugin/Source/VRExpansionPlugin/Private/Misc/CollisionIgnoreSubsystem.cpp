67b2eda030be15d261af4222760de24b6c9dc343
mordentral <morden@noxhub.net>
2025-01-20 18:24:55 -0500

    fix collision ignore subsystem not correctly deleting if invalid primitives with chaos

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/Misc/CollisionIgnoreSubsystem.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/Misc/CollisionIgnoreSubsystem.cpp
@@ -150,17 +150,14 @@ void UCollisionIgnoreSubsystem::CheckActiveFilters()
 {
 	bool bMadeChanges = false;
 
-	for (TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : CollisionTrackedPairs)
+	for (TMap<FCollisionPrimPair, FCollisionIgnorePairArray>::TIterator ItRemove = CollisionTrackedPairs.CreateIterator(); ItRemove; ++ItRemove)
 	{
-		// First check for invalid primitives
-		if (!IsValid(KeyPair.Key.Prim1) || !IsValid(KeyPair.Key.Prim2))
+		// First check for invalid primitives and an empty pair array
+		if (!IsValid(ItRemove->Key.Prim1) || !IsValid(ItRemove->Key.Prim2) || ItRemove->Value.PairArray.Num() < 1)
 		{
-			// If we don't have a map element for this pair, then add it now
-			if (!RemovedPairs.Contains(KeyPair.Key))
-			{
-				RemovedPairs.Add(KeyPair.Key, KeyPair.Value);
-				bMadeChanges = true;
-			}
+			ItRemove->Value.PairArray.Empty();
+			ItRemove.RemoveCurrent();
+			bMadeChanges = true;
 
 			continue; // skip remaining checks as we have invalid primitives anyway
 		}
@@ -256,16 +253,6 @@ void UCollisionIgnoreSubsystem::CheckActiveFilters()
 				});
 		}
 #endif
-
-		// If there are no pairs left
-		if (KeyPair.Value.PairArray.Num() < 1)
-		{
-			// Try and remove it, chaos should be cleaning up the ignore setups
-			if (!RemovedPairs.Contains(KeyPair.Key))
-			{
-				RemovedPairs.Add(KeyPair.Key, KeyPair.Value);
-			}
-		}
 	}
 
 /*#if WITH_CHAOS
@@ -284,14 +271,14 @@ void UCollisionIgnoreSubsystem::CheckActiveFilters()
 	}
 #endif*/
 
-	for (const TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : RemovedPairs)
+	/*for (const TPair<FCollisionPrimPair, FCollisionIgnorePairArray>& KeyPair : RemovedPairs)
 	{
 		if (CollisionTrackedPairs.Contains(KeyPair.Key))
 		{
 			CollisionTrackedPairs[KeyPair.Key].PairArray.Empty();
 			CollisionTrackedPairs.Remove(KeyPair.Key);
 		}
-	}
+	}*/
 
 	UpdateTimer(bMadeChanges);
 }
