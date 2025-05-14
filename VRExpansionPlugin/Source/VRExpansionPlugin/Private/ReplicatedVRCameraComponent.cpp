ac73ab40ec8ec6c5bbb3b705e3af1074750cdb88
mordentral <morden@noxhub.net>
2025-05-13 22:37:12 -0400

    Fix seated offset issues

--- VRExpansionPlugin/Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Private/ReplicatedVRCameraComponent.cpp
@@ -210,7 +210,7 @@ void UReplicatedVRCameraComponent::UpdateTracking(float DeltaTime)
 					FRotator StoredCameraRotOffset = FRotator::ZeroRotator;
 					if (AttachChar->VRMovementReference && AttachChar->VRMovementReference->GetReplicatedMovementMode() == EVRConjoinedMovementModes::C_VRMOVE_Seated)
 					{
-						AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
+						//StoredCameraRotOffset = AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
 					}
 					else
 					{
@@ -250,7 +250,7 @@ void UReplicatedVRCameraComponent::RunNetworkedSmoothing(float DeltaTime)
 		FRotator StoredCameraRotOffset = FRotator::ZeroRotator;
 		if (AttachChar->VRMovementReference && AttachChar->VRMovementReference->GetReplicatedMovementMode() == EVRConjoinedMovementModes::C_VRMOVE_Seated)
 		{
-			AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
+			//StoredCameraRotOffset = AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
 		}
 		else
 		{
@@ -485,7 +485,7 @@ void UReplicatedVRCameraComponent::HandleXRCamera()
 								FRotator StoredCameraRotOffset = FRotator::ZeroRotator;
 								if (AttachChar->VRMovementReference->GetReplicatedMovementMode() == EVRConjoinedMovementModes::C_VRMOVE_Seated)
 								{
-									AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
+									//StoredCameraRotOffset = AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
 								}
 								else
 								{
@@ -536,7 +536,7 @@ void UReplicatedVRCameraComponent::OnRep_ReplicatedCameraTransform()
 		FRotator StoredCameraRotOffset = FRotator::ZeroRotator;
 		if (AttachChar->VRMovementReference && AttachChar->VRMovementReference->GetReplicatedMovementMode() == EVRConjoinedMovementModes::C_VRMOVE_Seated)
 		{
-			AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
+			//StoredCameraRotOffset = AttachChar->SeatInformation.InitialRelCameraTransform.Rotator();
 		}
 		else
 		{
